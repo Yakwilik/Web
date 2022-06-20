fe0e976a66ee17d9a79ab283c8c0b958df0be001
Khasbulat <khas.2015@gmail.com>
2022-06-20 15:24:11 +0300

    dz2

--- askme/dz_2/askme/views.py
+++ askme/dz_2/askme/views.py
@@ -1,13 +1,59 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from paginator import paginate
 # Create your views here.
 
 QUESTIONS = [
     {
         "title": f"Title number {i}",
         "text": f"This is my question number {i}"
-    } for i in range(10 )
+    } for i in range(50)
 ]
 
+HOT_QUESTIONS = [
+    {
+        "title": f"Title of hot question number {i}",
+        "text": f"This is my hot question number {i}"
+    } for i in range(50)
+]
+
+first_row = {"1": "C++", "2": "Python", "3": "RUST"}
+second_row = {"1": "C#", "2": "QT", "3": "GoLang"}
+third_row = {"1": "Ruby", "2": "Java", "3": "PascalAbc"}
+
+TAGS = [first_row, second_row, third_row]
+
+MEMBERS = ["MeetUpTeam", "yakwilik", "vkTeam"]
+
+
+# TAGS = ["C++", "Python", "RUST", "C#", "SQL", "GoLang", "Ruby", "Java", "JavaScript", "PascalAbc"]
+
+
 def index(request):
-    return render(request, "index.html", {"questions": QUESTIONS})
+    page_questions = paginate(QUESTIONS, request, 5)
+    return render(request, "index.html", {"questions": page_questions, "tags": TAGS, "members": MEMBERS})
+
+
+def new_question(request):
+    return render(request, "new_question.html", {"questions": QUESTIONS, "tags": TAGS, "members": MEMBERS})
+
+
+def one_question(request, i: int):
+    return render(request, "one_question.html", {"questions": QUESTIONS,
+                                                 "tags": TAGS, "members": MEMBERS})
+
+
+def registration(request):
+    return render(request, "registr.html", {"questions": QUESTIONS,
+                                                 "tags": TAGS, "members": MEMBERS})
+
+
+def authentication(request):
+    return render(request, "auth.html", {"questions": QUESTIONS,
+                                                 "tags": TAGS, "members": MEMBERS})
+
+
+def hot_question(request):
+    page_questions = paginate(HOT_QUESTIONS, request, 5)
+    return render(request, "hot.html", {"questions": page_questions, "tags": TAGS, "members": MEMBERS})
+
+
